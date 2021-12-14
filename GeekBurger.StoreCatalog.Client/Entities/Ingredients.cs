4c65ab02f1d2bc5a94f3de156b652abe6209c15b
Rafael Graziosi <rafael.graziosi@gmail.com>
2021-12-14 20:03:23 -0300

    criação de entidades de chamadas https para microserviços

---
+++ GeekBurger.StoreCatalog.Client/Entities/Ingredients.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekBurger.StoreCatalog.Client.Entities
+{
+    internal class Ingredients
+    {
+        public List<string> Restrictions { get; set; }
+        public String StoreName { get; set; }
+
+        public Ingredients()
+        {
+            Restrictions = new List<string>();
+        }
+    }
+
+
+}
