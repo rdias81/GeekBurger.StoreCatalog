c954262c1bdf277112ab1dee6158258f430fbb64
Renan Rodrigues <r.rodrigues@avivatec.com.br>
2021-12-16 21:10:53 -0300

    Cache memory

---
+++ GeekBurger.StoreCatalog.DataCache/IMemoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekBurger.StoreCatalog.DataCache
+{
+    public interface IMemoryRepository
+    {
+        object GetById(int id);
+        void RemoveById(int id);
+
+        object Add(object objeto);
+
+    }
+}
